460ef3fdb14315082b67ea4f4beb95f67642871a
LegrosGabriel <133345446+LegrosGabriel@users.noreply.github.com>
2024-05-03 22:13:07 +0200

    Update utilisateur.php

--- utilisateur.php
+++ utilisateur.php
@@ -10,46 +10,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CY-Rencontres</title>
-    <link rel="stylesheet" type="text/CSS" href="utilisateur.css">
-    <script src="utilisateur.js"></script>
+    <link rel="stylesheet" type="text/CSS" href="Uparametre.css">
+    <script src="Uparametre.js"></script>
 </head>
 <body>
-
-    <div id="containerA">
+   <div id="containerA">
         <span id="Accueil" class="bouton"> <a href="utilisateur.php">Accueil</a></span>
         <span id="Profil" class="bouton"> <a href="Uprofil.php">Profil</a></span>
         <span id="Messagerie" class="bouton"> <a href="Umessagerie.php">Messagerie</a></span>
-        <span id="Parametre" class="bouton"> <a href="Uparametre.php">Paramètre</a></span>
+        <span id="Parametres" class="bouton"> <a href="Uparametre.php">Paramètres</a></span>
+        <span id="Deconnexion" class="bouton"> <a href="acceuil.html">Déconnexion</a></span>
     </div>
 
+      <h1>Paramètres</h1>
 
 
 
 
-      <h1>Bienvenue <?= $_SESSION['pseudo'] ?> </h1>
 
-    
-    
+     
 
-      <?php
-      /*
-      //lecture du fichier ligne par ligne et s'arrete a la fin quand fgetcsv renvoie faux
-      while (($ligne = fgetcsv($fichier)) !== false) {
 
-            //Verification pour presenter le sexe oppose
-            if($_SESSION["sexe"] != $ligne[4]){
-              //Afficher cet utilisateur pseudo + description
-              //Crer un "dictionnaire" ou on va mettre ces personnes pour les recuperer plus tard dans la page
-              global $user_print = (
-                "pseudo" => $ligne[2];
-                "description" => $ligne[9];
-              );
-          }
-      }
-      */  
-      ?>
-
-      <!--<p><b><?= $user_print['pseudo'] ?></b></p>-->
 
 </body>
 </html>
