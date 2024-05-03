1b3e3c2d173f5d1e5944d3e522270df09d39d478
LegrosGabriel <133345446+LegrosGabriel@users.noreply.github.com>
2024-05-03 21:30:49 +0200

    Création
    structure pages utilisateurs

---
+++ Umessagerie.php
@@ -0,0 +1,35 @@
+<?php
+    session_start();
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CY-Rencontres</title>
+    <link rel="stylesheet" type="text/CSS" href="Umessagerie.css">
+    <script src="Umessagerie.js"></script>
+</head>
+<body>
+    <div id="containerA">
+        <span id="Accueil" class="bouton"> <a href="utilisateur.php">Accueil</a></span>
+        <span id="Profil" class="bouton"> <a href="Uprofil.php">Profil</a></span>
+        <span id="Messagerie" class="bouton"> <a href="Umessagerie.php">Messagerie</a></span>
+        <span id="Parametre" class="bouton"> <a href="Uparametre.php">Paramètre</a></span>
+    </div>
+
+      <h1>Messagerie</h1>
+
+
+
+
+
+     
+
+
+
+</body>
+</html>
---
+++ Uparametre.php
@@ -0,0 +1,35 @@
+<?php
+    session_start();
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CY-Rencontres</title>
+    <link rel="stylesheet" type="text/CSS" href="Uparametre.css">
+    <script src="Uparametre.js"></script>
+</head>
+<body>
+    <div id="containerA">
+        <span id="Accueil" class="bouton"> <a href="utilisateur.php">Accueil</a></span>
+        <span id="Profil" class="bouton"> <a href="Uprofil.php">Profil</a></span>
+        <span id="Messagerie" class="bouton"> <a href="Umessagerie.php">Messagerie</a></span>
+        <span id="Parametre" class="bouton"> <a href="Uparametre.php">Paramètre</a></span>
+    </div>
+
+      <h1>Paramètre</h1>
+
+
+
+
+
+     
+
+
+
+</body>
+</html>
