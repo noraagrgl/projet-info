99b6f971a6a810480b20ff7239cea9ae8c9a02b7
LegrosGabriel <133345446+LegrosGabriel@users.noreply.github.com>
2024-05-07 09:10:22 +0200

    Update Umessagerie.php

--- Umessagerie.php
+++ Umessagerie.php
@@ -1,8 +1,9 @@
 <?php
     session_start();
-     if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: acceuil.html");
     exit;
+    }
 ?>
 
 
