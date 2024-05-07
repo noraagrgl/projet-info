1dd3833e69dea8f49190e404177f315a502bc1c9
LegrosGabriel <133345446+LegrosGabriel@users.noreply.github.com>
2024-05-07 08:35:39 +0200

    Update Uparametre.php

--- Uparametre.php
+++ Uparametre.php
@@ -1,5 +1,9 @@
 <?php
     session_start();
+     if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: acceuil.html");
+    exit;
+
    // Vérifie si le formulaire a été soumis
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
