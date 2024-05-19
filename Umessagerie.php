0fc8a372f133adf075218d8876339e8b4e5dcc74
LegrosGabriel <133345446+LegrosGabriel@users.noreply.github.com>
2024-05-19 08:50:26 +0200

    Update Umessagerie.php

--- Umessagerie.php
+++ Umessagerie.php
@@ -1,11 +1,17 @@
 <?php
     session_start();
     
+
+
     if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: acceuil.html");
     exit;
-}
+   
+    }
 
+    if($_SESSION['abonnement']=='gratuit'){
+      header("location: utilisateur.php");
+    }
 ?>
 
 <!DOCTYPE html>
@@ -14,17 +20,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CY-Rencontres</title>
-    <link rel="stylesheet" type="text/CSS" href="Umessagerie.css">
+    <link rel="stylesheet" type="text/CSS" href="js-css/Umessagerie.css">
     <!-- <sript src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></sript> -->
-    <!--<script type="text/javascript" src="Umessagerie.js"></script>-->
+    <!--<script type="text/javascript" src="js-css/Umessagerie.js"></script>-->
 </head>
 <body>
   <div id="containerA">
-        <span id="Accueil" class="bouton"> <a href="utilisateur.php">Accueil</a></span>
-        <span id="Profil" class="bouton"> <a href="Uprofil.php">Profil</a></span>
-        <span id="Messagerie" class="bouton"> <a href="Umessagerie.php">Messagerie</a></span>
-        <span id="Parametres" class="bouton"> <a href="Uparametre.php">Paramètres</a></span>
-        <span id="Deconnexion" class="bouton"> <a href="deconnexion.php">Déconnexion</a></span>
+      <div class="bouton">
+        <img src="image/accueil.png" alt="image d'accueil" class="imageSelection"/>
+        <span id="Accueil" class="bouton"> <a href="utilisateur.php" class="a">Accueil</a></span>
+      </div>
+      <div class="bouton">
+        <img src="image/profil.png" alt="image profil" class="imageSelection"/>
+        <span id="Profil" class="bouton"> <a href="Uprofil.php" class="a">Profil</a></span>
+      </div>
+      <div class="bouton">
+        <img src="image/messagerieIcone.png" alt="image messagerie" class="imageSelection"/>
+        <span id="Messagerie" class="bouton"> <a href="Umessagerie.php" class="a">Messagerie</a></span>
+      </div>
+      <div class="bouton">
+        <img src="image/parametre.png" alt="image parametre" class="imageSelection"/>
+        <span id="Parametres" class="bouton"> <a href="Uparametre.php" class="a">Paramètres</a></span>
+      </div>
+      <div class="bouton">
+         
+        <img src="image/deconnexion.png" alt="image deconnexion" class="imageSelection"/> 
+        <span id="Deconnexion" class="bouton"> <a href="deconnexion.php" class="a">Déconnexion</a></span>
+      </div>
+        
     </div>
 
     <div id="containerMessagerie">
@@ -36,7 +59,7 @@
 
       <?php
       
-      $fichier=fopen("utilisateurs.txt", "r");
+      $fichier=fopen("data/utilisateurs.txt", "r");
       //lecture du fichier ligne par ligne et s'arrete a la fin quand fgetcsv renvoie faux
       while (($ligne = fgets($fichier)) !== false) {
 
@@ -103,7 +126,7 @@
 
   
   <sript src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></sript>
-  <script type="text/javascript" src="Umessagerie.js"></script>
+  <script type="text/javascript" src="js-css/Umessagerie.js"></script>
 
 
 
