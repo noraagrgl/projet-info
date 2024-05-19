715e1bb9283c84803bc38fae0f4d62f82df6fd50
LegrosGabriel <133345446+LegrosGabriel@users.noreply.github.com>
2024-05-19 08:51:00 +0200

    Update Uparametre.php

--- Uparametre.php
+++ Uparametre.php
@@ -15,16 +15,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CY-Rencontres</title>
-    <link rel="stylesheet" type="text/CSS" href="Uprofil.css">
-    <script src="Uprofil.js"></script>
+    <link rel="stylesheet" type="text/CSS" href="js-css/Uprofil.css">
+    <script src="js-css/Uprofil.js"></script>
 </head>
 <body>
     <div id="containerA">
-        <span id="Accueil" class="bouton"> <a href="utilisateur.php">Accueil</a></span>
-        <span id="Profil" class="bouton"> <a href="Uprofil.php">Profil</a></span>
-        <span id="Messagerie" class="bouton"> <a href="Umessagerie.php">Messagerie</a></span>
-        <span id="Parametres" class="bouton"> <a href="Uparametre.php">Paramètres</a></span>
-        <span id="Deconnexion" class="bouton"> <a href="deconnexion.php">Déconnexion</a></span>
+      <div class="bouton">
+        <img src="image/accueil.png" alt="image d'accueil" class="imageSelection"/>
+        <span id="Accueil" class="bouton"> <a href="utilisateur.php" class="a">Accueil</a></span>
+      </div>
+      <div class="bouton">
+        <img src="image/profil.png" alt="image profil" class="imageSelection"/>
+        <span id="Profil" class="bouton"> <a href="Uprofil.php" class="a">Profil</a></span>
+      </div>
+      <div class="bouton">
+        <img src="image/messagerieIcone.png" alt="image messagerie" class="imageSelection"/>
+        <span id="Messagerie" class="bouton"> <a href="Umessagerie.php" class="a">Messagerie</a></span>
+      </div>
+      <div class="bouton">
+        <img src="image/parametre.png" alt="image parametre" class="imageSelection"/>
+        <span id="Parametres" class="bouton"> <a href="Uparametre.php" class="a">Paramètres</a></span>
+      </div>
+      <div class="bouton">
+         
+        <img src="image/deconnexion.png" alt="image deconnexion" class="imageSelection"/> 
+        <span id="Deconnexion" class="bouton"> <a href="deconnexion.php" class="a">Déconnexion</a></span>
+      </div>
+        
     </div>
 
       <h1>Paramètres</h1>
