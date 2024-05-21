370ebae5af2bf1de872083d6e66525510f703c40
LegrosGabriel <133345446+LegrosGabriel@users.noreply.github.com>
2024-05-21 09:15:06 +0200

    Update Umessagerie.php

--- Umessagerie.php
+++ Umessagerie.php
@@ -26,67 +26,85 @@
 </head>
 <body>
   <div id="containerA">
+        
       <div class="bouton">
         <img src="image/accueil.png" alt="image d'accueil" class="imageSelection"/>
         <span id="Accueil" class="bouton"> <a href="utilisateur.php" class="a">Accueil</a></span>
       </div>
+
+      <div class="bouton">
+        <img src="image/loupe.jpg" alt="image recherhce" class="imageSelection"/>
+        <span id="Profil" class="bouton"> <a href="Urecherche.php">Recherche</a></span>
+      </div>
+
       <div class="bouton">
         <img src="image/profil.png" alt="image profil" class="imageSelection"/>
         <span id="Profil" class="bouton"> <a href="Uprofil.php" class="a">Profil</a></span>
       </div>
+
       <div class="bouton">
         <img src="image/messagerieIcone.png" alt="image messagerie" class="imageSelection"/>
         <span id="Messagerie" class="bouton"> <a href="Umessagerie.php" class="a">Messagerie</a></span>
       </div>
+
       <div class="bouton">
         <img src="image/parametre.png" alt="image parametre" class="imageSelection"/>
         <span id="Parametres" class="bouton"> <a href="Uparametre.php" class="a">Paramètres</a></span>
       </div>
-      <div class="bouton">
-         
+
+      <div class="bouton" id="adminDIV"></div>
+
+      <div class="bouton">       
         <img src="image/deconnexion.png" alt="image deconnexion" class="imageSelection"/> 
         <span id="Deconnexion" class="bouton"> <a href="deconnexion.php" class="a">Déconnexion</a></span>
       </div>
         
-    </div>
 
     <div id="containerMessagerie">
       <div id="contacts">
         <h2>CONTACTS</h2>
         <div id="contactErreur"></div>
+          <!-- <form action="UformulaireBlocage.php" method="POST"> -->
 
 
 
-      <?php
-      
-      $fichier=fopen("data/utilisateurs.txt", "r");
-      //lecture du fichier ligne par ligne et s'arrete a la fin quand fgetcsv renvoie faux
-      while (($ligne = fgets($fichier)) !== false) {
+            <?php
+                  
+              $fichier2 = fopen("data/bannissement.txt", "r");
+              $bannis = [];
+              while (($email = fgets($fichier2)) !== false) {
+                  $bannis[] = trim($email);
+              }
+              fclose($fichier2);
 
-          $ligne = explode(";", trim($ligne));
+              $fichier = fopen("data/utilisateurs.txt", "r");
 
-          if(isset($ligne[4]) && isset($ligne[8])) {
-             //Verification pour presenter le sexe oppose
-              if($_SESSION["sexe"] != $ligne[4]){
-                  //Verification pour presenter le meme type de relation
-                  if($_SESSION["type_relation"] == $ligne[8]){
-                      //Afficher cet utilisateur pseudo + description
-                      ?>
-                      <!--<div id="user_print" onclick="contact(this)">-->
-                      <div class="user_print active" data-id="<?= $ligne[0] ?>"><!--On peut rajouter d'autres champs si on le souhaite -->
-                          <p><img src="image/<?= $ligne[10] ?>" class="image"><b><?= $ligne[2] ?></b></p>
-                      </div>
-                      <?php
+              //Lecture du fichier ligne par ligne
+              while (($ligne = fgets($fichier)) !== false) {
+                  $ligne = explode(";", trim($ligne));
+
+                  //Vérification des indices et !=sexe et ==type_relation
+                  if (isset($ligne[4]) && isset($ligne[8])) {
+                      if ($_SESSION["sexe"] != $ligne[4] && $_SESSION["type_relation"] == $ligne[8]) {
+                          //Verification si l'utilisateur est banni
+                          if (!in_array($ligne[0], $bannis)) {
+                        ?>
+                        <!--<div id="user_print" onclick="contact(this)">-->
+                        <div class="user_print active" data-id="<?= $ligne[0] ?>"><!--On peut rajouter d'autres champs si on le souhaite -->
+                            <p><img src="image/<?= $ligne[10] ?>" class="image"><b><?= $ligne[2] ?>   <span class="boutonBloquer" ><button type="submit">Bloquer utilisateur</button></b></p></span>
+                        </div>
+                        <?php
+                          }
+                      }
                   }
               }
-          }
-      }
-      
-      ?>
+              fclose($fichier);
+
+            ?>
 
 
 
-
+      <!-- </form> -->
       </div>
 
         
