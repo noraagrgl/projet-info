a8c112c1d33a26f30ca61d27fe6f5b032b700920
LegrosGabriel <133345446+LegrosGabriel@users.noreply.github.com>
2024-05-21 09:06:45 +0200

    Update Ugestion.php

--- Ugestion.php
+++ Ugestion.php
@@ -19,27 +19,40 @@
 </head>
 <body>
 
-    <div id="containerA">
-        <div class="bouton">
-            <img src="image/accueil.png" alt="image d'accueil" class="imageSelection"/>
-            <span id="Accueil" class="bouton"> <a href="utilisateur.php" class="a">Accueil</a></span>
-        </div>
-        <div class="bouton">
-            <img src="image/profil.png" alt="image profil" class="imageSelection"/>
-            <span id="Profil" class="bouton"> <a href="Uprofil.php" class="a">Profil</a></span>
-        </div>
-        <div class="bouton">
-            <img src="image/messagerieIcone.png" alt="image messagerie" class="imageSelection"/>
-            <span id="Messagerie" class="bouton"> <a href="Umessagerie.php" class="a">Messagerie</a></span>
-        </div>
-        <div class="bouton">
-            <img src="image/parametre.png" alt="image parametre" class="imageSelection"/>
-            <span id="Parametres" class="bouton"> <a href="Uparametre.php" class="a">Paramètres</a></span>
-        </div>
-        <div class="bouton">
-            <img src="image/deconnexion.png" alt="image deconnexion" class="imageSelection"/> 
-            <span id="Deconnexion" class="bouton"> <a href="deconnexion.php" class="a">Déconnexion</a></span>
-        </div>
+     <div id="containerA">
+        
+      <div class="bouton">
+        <img src="image/accueil.png" alt="image d'accueil" class="imageSelection"/>
+        <span id="Accueil" class="bouton"> <a href="utilisateur.php" class="a">Accueil</a></span>
+      </div>
+
+      <div class="bouton">
+        <img src="image/loupe.jpg" alt="image recherhce" class="imageSelection"/>
+        <span id="Profil" class="bouton"> <a href="Urecherche.php">Recherche</a></span>
+      </div>
+
+      <div class="bouton">
+        <img src="image/profil.png" alt="image profil" class="imageSelection"/>
+        <span id="Profil" class="bouton"> <a href="Uprofil.php" class="a">Profil</a></span>
+      </div>
+
+      <div class="bouton">
+        <img src="image/messagerieIcone.png" alt="image messagerie" class="imageSelection"/>
+        <span id="Messagerie" class="bouton"> <a href="Umessagerie.php" class="a">Messagerie</a></span>
+      </div>
+
+      <div class="bouton">
+        <img src="image/parametre.png" alt="image parametre" class="imageSelection"/>
+        <span id="Parametres" class="bouton"> <a href="Uparametre.php" class="a">Paramètres</a></span>
+      </div>
+
+      <div class="bouton" id="adminDIV"></div>
+
+      <div class="bouton">       
+        <img src="image/deconnexion.png" alt="image deconnexion" class="imageSelection"/> 
+        <span id="Deconnexion" class="bouton"> <a href="deconnexion.php" class="a">Déconnexion</a></span>
+      </div>
+        
     </div>
 
     <div id="listeUtilisateur">
@@ -61,6 +74,39 @@
             ?>
         </ul>
     </div>
+
+
+    <div id="signalement">
+    <h2>Utilisateur signalé</h2>
+    <?php
+        // Ouvrir le fichier "signalement.txt"
+        $fichier2 = fopen("data/signalement.txt", "r");
+
+        // Vérifier si le fichier a été ouvert correctement
+        if ($fichier2) {
+            // Lire chaque ligne du fichier
+            while (($Ligne = fgets($fichier2)) !== false) {
+                // Diviser la ligne en éléments du tableau
+                $Ligne = explode(";", trim($Ligne));
+                ?>
+                <li>
+                    <p><b>Emmeteur du signalement</b> <?= htmlspecialchars($Ligne[0]) ?>, 
+                       <b>recepteur du signalement</b> <?= htmlspecialchars($Ligne[1]) ?>, 
+                       <b>le message est</b> <?= htmlspecialchars($Ligne[2]) ?> 
+                       <b>et le motif du signalement</b> <?= htmlspecialchars($Ligne[4]) ?></p>
+                </li>
+                <?php
+            }
+            // Fermer le fichier après lecture
+            fclose($fichier2);
+        } else {
+            // Afficher un message si le fichier ne peut pas être ouvert
+            echo "Impossible d'ouvrir le fichier signalement.txt.";
+        }
+    ?>
+</div>
+
+
 
     <script src="js-css/Ugestion.js"></script>
 </body>
