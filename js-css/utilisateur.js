12bc8c70fa40149116c1839dc2e2649876a45274
LegrosGabriel <133345446+LegrosGabriel@users.noreply.github.com>
2024-05-21 09:02:16 +0200

    Update utilisateur.js

--- js-css/utilisateur.js
+++ js-css/utilisateur.js
@@ -1,19 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
-
-    var gestionDiv= document.getElementById("gestionDiv");
+    var gestionDiv = document.getElementById("gestionDiv");
+    var adminDIV = document.getElementById("adminDIV");
 
     if (abonnement == "admin") {
+        
+
+        var imageElement = document.createElement("img");
+        imageElement.src = "image/admin.png";
+        imageElement.alt = "image admin";
+        imageElement.classList.add("imageSelection"); // Corrected class addition
+        adminDIV.appendChild(imageElement);
+
+
 
         var linkElement = document.createElement("a");
         linkElement.href = "Ugestion.php"; 
         linkElement.textContent = "Gestion admin";
         linkElement.style.display = "inline-block";
         linkElement.style.marginTop = "10px";
-        gestionDiv.appendChild(linkElement);
-
-        
+        linkElement.id = "idGestionAdmin";
+        adminDIV.appendChild(linkElement);
     }
-
-
-    
 });
