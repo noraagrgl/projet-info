3268c44961cce00e6dfc7d5324c979565da5f68b
LegrosGabriel <133345446+LegrosGabriel@users.noreply.github.com>
2024-05-21 09:12:20 +0200

    Update UconsulterProfil.php

--- UconsulterProfil.php
+++ UconsulterProfil.php
@@ -53,26 +53,39 @@
 </head>
 <body>
     <div id="containerA">
-        <div class="bouton">
-            <img src="image/accueil.png" alt="image d'accueil" class="imageSelection"/>
-            <span id="Accueil" class="bouton"> <a href="utilisateur.php" class="a">Accueil</a></span>
-        </div>
-        <div class="bouton">
-            <img src="image/profil.png" alt="image profil" class="imageSelection"/>
-            <span id="Profil" class="bouton"> <a href="Uprofil.php" class="a">Profil</a></span>
-        </div>
-        <div class="bouton">
-            <img src="image/messagerieIcone.png" alt="image messagerie" class="imageSelection"/>
-            <span id="Messagerie" class="bouton"> <a href="Umessagerie.php" class="a">Messagerie</a></span>
-        </div>
-        <div class="bouton">
-            <img src="image/parametre.png" alt="image parametre" class="imageSelection"/>
-            <span id="Parametres" class="bouton"> <a href="Uparametre.php" class="a">Paramètres</a></span>
-        </div>
-        <div class="bouton">
-            <img src="image/deconnexion.png" alt="image deconnexion" class="imageSelection"/> 
-            <span id="Deconnexion" class="bouton"> <a href="deconnexion.php" class="a">Déconnexion</a></span>
-        </div>
+        
+      <div class="bouton">
+        <img src="image/accueil.png" alt="image d'accueil" class="imageSelection"/>
+        <span id="Accueil" class="bouton"> <a href="utilisateur.php" class="a">Accueil</a></span>
+      </div>
+
+      <div class="bouton">
+        <img src="image/loupe.jpg" alt="image recherhce" class="imageSelection"/>
+        <span id="Profil" class="bouton"> <a href="Urecherche.php">Recherche</a></span>
+      </div>
+
+      <div class="bouton">
+        <img src="image/profil.png" alt="image profil" class="imageSelection"/>
+        <span id="Profil" class="bouton"> <a href="Uprofil.php" class="a">Profil</a></span>
+      </div>
+
+      <div class="bouton">
+        <img src="image/messagerieIcone.png" alt="image messagerie" class="imageSelection"/>
+        <span id="Messagerie" class="bouton"> <a href="Umessagerie.php" class="a">Messagerie</a></span>
+      </div>
+
+      <div class="bouton">
+        <img src="image/parametre.png" alt="image parametre" class="imageSelection"/>
+        <span id="Parametres" class="bouton"> <a href="Uparametre.php" class="a">Paramètres</a></span>
+      </div>
+
+      <div class="bouton" id="adminDIV"></div>
+
+      <div class="bouton">       
+        <img src="image/deconnexion.png" alt="image deconnexion" class="imageSelection"/> 
+        <span id="Deconnexion" class="bouton"> <a href="deconnexion.php" class="a">Déconnexion</a></span>
+      </div>
+        
     </div>
 
 
