0ecfdf8924d755d9a5ece21e6141087b9ed56536
LegrosGabriel <133345446+LegrosGabriel@users.noreply.github.com>
2024-05-21 09:01:55 +0200

    Update utilisateur.php

--- utilisateur.php
+++ utilisateur.php
@@ -23,38 +23,42 @@
 <body>
 
     <div id="containerA">
+        
       <div class="bouton">
         <img src="image/accueil.png" alt="image d'accueil" class="imageSelection"/>
         <span id="Accueil" class="bouton"> <a href="utilisateur.php" class="a">Accueil</a></span>
       </div>
+
+      <div class="bouton">
+        <img src="image/loupe.jpg" alt="image recherhce" class="imageSelection"/>
+        <span id="Profil" class="bouton"> <a href="Urecherche.php">Recherche</a></span>
+      </div>
+
       <div class="bouton">
         <img src="image/profil.png" alt="image profil" class="imageSelection"/>
         <span id="Profil" class="bouton"> <a href="Uprofil.php" class="a">Profil</a></span>
       </div>
-
-
 
       <div class="bouton">
         <img src="image/messagerieIcone.png" alt="image messagerie" class="imageSelection"/>
         <span id="Messagerie" class="bouton"> <a href="Umessagerie.php" class="a">Messagerie</a></span>
       </div>
 
-
-
-
       <div class="bouton">
         <img src="image/parametre.png" alt="image parametre" class="imageSelection"/>
         <span id="Parametres" class="bouton"> <a href="Uparametre.php" class="a">Paramètres</a></span>
       </div>
-      <div class="bouton">
-         
+
+      <div class="bouton" id="adminDIV"></div>
+
+      <div class="bouton">       
         <img src="image/deconnexion.png" alt="image deconnexion" class="imageSelection"/> 
         <span id="Deconnexion" class="bouton"> <a href="deconnexion.php" class="a">Déconnexion</a></span>
       </div>
         
     </div>
 
-    <a href="Urecherche.php">Recherche</a>
+    
 
     <div id="gestionDiv"></div>
 
@@ -64,43 +68,38 @@
 
 
       <?php
+            
+            $fichier2 = fopen("data/bannissement.txt", "r");
+            $bannis = [];
+            while (($email = fgets($fichier2)) !== false) {
+                $bannis[] = trim($email);
+            }
+            fclose($fichier2);
 
-      $fichier=fopen("data/utilisateurs.txt", "r");
+            $fichier = fopen("data/utilisateurs.txt", "r");
 
-      //lecture du fichier ligne par ligne et s'arrete a la fin quand fgetcsv renvoie faux
-      while (($ligne = fgets($fichier)) !== false) {
+            //Lecture du fichier ligne par ligne
+            while (($ligne = fgets($fichier)) !== false) {
+                $ligne = explode(";", trim($ligne));
 
-            $ligne = explode(";", trim($ligne));
-
-            if(isset($ligne[4]) && isset($ligne[8])) {
-
-                //Verification pour presenter le sexe oppose
-                if($_SESSION["sexe"] != $ligne[4]){
-                    //Verification pour presenter le meme type de relation
-                    if($_SESSION["type_relation"] == $ligne[8]){
-                        //Afficher cet utilisateur pseudo + description
-
-                        ?>
-
-                        <div id="user_print">
-                            <p><img src="image/<?= $ligne[10] ?>" class="image"><b><?= $ligne[2] ?></b></p>
-                            <p><?= $ligne[9] ?></p>
-                            <a href="Umessagerie.php">Envoyer un message</a>
-                        </div>
-
-
-
-                        <?php
-
-
-
+                //Vérification des indices et !=sexe et ==type_relation
+                if (isset($ligne[4]) && isset($ligne[8])) {
+                    if ($_SESSION["sexe"] != $ligne[4] && $_SESSION["type_relation"] == $ligne[8]) {
+                        //Verification si l'utilisateur est banni
+                        if (!in_array($ligne[0], $bannis)) {
+                            ?>
+                                <div id="user_print">
+                                    <p><img src="image/<?= $ligne[10] ?>" class="image"><b><?= $ligne[2] ?></b></p>
+                                    <p><?= $ligne[9] ?></p>
+                                    <a href="Umessagerie.php">Envoyer un message</a>
+                                </div>
+                            <?php
+                        }
                     }
-
                 }
             }
-      }
-
-      ?>
+            fclose($fichier);
+        ?>
 
       <!--<p><b><?= $user_print['pseudo'] ?></b></p>-->
 
