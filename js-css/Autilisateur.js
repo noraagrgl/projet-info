6d1fe7c18118e8a5259204b838b71a45aac7ad4c
LegrosGabriel <133345446+LegrosGabriel@users.noreply.github.com>
2024-05-26 20:58:01 +0200

    Update Autilisateur.js

--- js-css/Autilisateur.js
+++ js-css/Autilisateur.js
@@ -44,6 +44,97 @@ var xhttp = new XMLHttpRequest();
 */
 
 
+//___________________________________________________________________________
+//                         SUPRESSION COMPTE AJAX
+//___________________________________________________________________________
+
+function supprimerCompte(emailUtilisateur){
+
+var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+        }
+    };
+
+    
+
+    xhttp.open("POST", "AsuppressionCompte.php", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("email=" + emailUtilisateur);
+
+}
+
+//___________________________________________________________________________
+//                         BANISSEMENT COMPTE AJAX
+//___________________________________________________________________________
+
+function bannirCompte(emailUtilisateur){
+
+var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+        }
+    };
+
+    
+
+    xhttp.open("POST", "Abannissement.php", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("email=" + emailUtilisateur);
+
+}
+
+
+
+//___________________________________________________________________________
+//                         ESTBANNI COMPTE AJAX
+//___________________________________________________________________________
+
+function estBanni(emailUtilisateur){
+
+var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+        }
+    };
+
+    
+
+    xhttp.open("POST", "AutilisateurBanni.php", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("email=" + emailUtilisateur);
+
+}
+
+//___________________________________________________________________________
+//                         BANISSEMENT COMPTE AJAX
+//___________________________________________________________________________
+
+function debannirCompte(emailUtilisateur){
+
+var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+        }
+    };
+
+    
+
+    xhttp.open("POST", "Adebannissement.php", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("email=" + emailUtilisateur);
+
+}
+
+
+
+
+
+
 
 
 
@@ -94,6 +185,8 @@ document.querySelectorAll('.user_print').forEach(item => {
                 var dateEnvoie = messageData[3];
                 var emailSession = messageData[4]; // Email de la session
 
+                
+
                 if ((userId == emmeteur && emailSession == destinataire) || (userId == destinataire && emailSession == emmeteur)) {
                     var messageElement = document.createElement("div");
 
@@ -104,52 +197,75 @@ document.querySelectorAll('.user_print').forEach(item => {
                     messageElement.style.display = "inline-block";
                     messageElement.style.border = "1px solid black";
                     messageElement.style.borderRadius = "20px";
-                    messageElement.style.width = "800px";
+                    messageElement.style.width = "520px";
                     messageElement.style.marginTop = "10px";
                     messageElement.style.padding = "15px";
 
                     if (userId == emmeteur) {
-                        messageElement.style.float = "left";
-                        messageElement.style.marginRight = "600px";
+                        messageElement.style.float = "right";
+                        messageElement.style.marginRight = "100px";
                         messageElement.style.marginLeft = "125px";
                         messageElement.style.backgroundColor = "hotpink";
                     } else {
-                        messageElement.style.float = "right";
-                        messageElement.style.marginLeft = "600px";
+                        messageElement.style.float = "left";
+                        messageElement.style.marginLeft = "100px";
                         messageElement.style.marginRight = "125px";
                         messageElement.style.backgroundColor = "white";
                     }
 
+                    messageElement.messageClicked = false;
+
                     messageElement.addEventListener("click", function() {
-                        var dateElement = document.createElement("div");
-                        dateElement.textContent = dateEnvoie;
-                        dateElement.style.fontSize = "12px";
-                        dateElement.style.float = "right";
-                        dateElement.style.color = "gray";
-                        dateElement.style.marginTop = "5px";
-                        dateElement.style.padding = "5px";
-                        messageElement.appendChild(dateElement);
 
-                        if (userId == destinataire) {
-                            var suppr = document.createElement("img");
-                            suppr.src = "image/poubelle.png";
-                            suppr.alt = "logo d'une poubelle";
-                            suppr.style.objectFit = "cover";
-                            suppr.style.width = "25px";
-                            suppr.style.height = "25px";
-                            suppr.style.borderRadius = "50%";
-                            suppr.style.cursor = "pointer";
 
-                            messageElement.appendChild(suppr);
+                        if(!messageElement.messageClicked){
 
-                            suppr.addEventListener("click", function(event) {
-                                event.stopPropagation();
-                                console.log("dateEnvoie = " + dateEnvoie);
-                                supprimerMessage(dateEnvoie);
-                            });
+                            var dateElement = document.createElement("div");
+                            dateElement.textContent = dateEnvoie;
+                            dateElement.style.fontSize = "12px";
+                            dateElement.style.float = "right";
+                            dateElement.style.color = "gray";
+                            dateElement.style.marginTop = "5px";
+                            dateElement.style.padding = "5px";
+                            messageElement.appendChild(dateElement);
+
+                            if (userId == destinataire) {
+                                var suppr = document.createElement("img");
+                                suppr.src = "image/poubelle.png";
+                                suppr.alt = "logo d'une poubelle";
+                                suppr.style.objectFit = "cover";
+                                suppr.style.width = "25px";
+                                suppr.style.height = "25px";
+                                suppr.style.borderRadius = "50%";
+                                suppr.style.cursor = "pointer";
+
+                                messageElement.appendChild(suppr);
+
+                                suppr.addEventListener("click", function(event) {
+                                    event.stopPropagation();
+                                    console.log("dateEnvoie = " + dateEnvoie);
+                                    supprimerMessage(dateEnvoie);
+                                });
+
+
+                            }
+
+                            messageElement.messageClicked = true;
+
                         }
+
+
+                        
                     });
                 }
+
+
+
+
+
+
+
+
             });
         })
         .catch(error => {
@@ -158,9 +274,89 @@ document.querySelectorAll('.user_print').forEach(item => {
     });
 });
 
+console.log("emailUtilisateur = "+emailUtilisateur);
+var supprimerProfil = document.getElementById("supprimerProfil");
+
+supprimerProfil.addEventListener("click", function(event) {
+    
+    var ok;
+    ok = confirm("Etes vous sûr de vouloir supprimer le compte de l'email "+emailUtilisateur+" ?");
+    if(ok==true){
+        supprimerCompte(emailUtilisateur);
+        alert("Le compte de "+emailUtilisateur+" à bien été supprimé.");
+        window.location.href = "Ugestion.php";
+    }
+
+
+});
+
+
+/*
+
+function verifierBannissement() {
+    var emailUtilisateur;
+   
+
+    const formData = new FormData();
+    formData.append('email', emailUtilisateur);
+
+    fetch('votre_script.php', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.utilisateurBanni) {
+            return true;
+        } else if (data.utilisateurBanni === false) {
+            return false;
+        } else {
+            console.log("Une erreur est survenue : " + data.error);
+        }
+    })
+    .catch(error => {
+        console.error('Erreur:', error);
+        console.log("Une erreur est survenue lors de la vérification.");
+    });
+}
+
+
+console.log("verifierBannissement = "+verifierBannissement());*/
+
+
+var bannirProfil = document.getElementById("bannirProfil");
+
+//estBanni(emailUtilisateur)
+bannirProfil.addEventListener("click", function(event) {
+
+    var ok;
+    ok = confirm("Etes vous sûr de vouloir bannir le compte de l'email "+emailUtilisateur+" ?");
+    if(ok==true){
+        bannirCompte(emailUtilisateur);
+        alert("Le compte de "+emailUtilisateur+" à bien été banni.");
+    }
+
+});
+
+var debannirProfil = document.getElementById("debannirProfil");
+
+debannirProfil.addEventListener("click", function(event) {
+
+    var ok;
+    ok = confirm("Etes vous sûr de vouloir débannir le compte de l'email "+emailUtilisateur+" ?");
+    if(ok==true){
+        debannirCompte(emailUtilisateur);
+        alert("Le compte de "+emailUtilisateur+" à bien été débanni.");
+    }
+
+});
 
 
 
 
 
 
+
+   
+
+    
