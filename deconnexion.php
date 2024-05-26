5ab2f1276819a82f9da02c447c40f819754826d4
noraagrgl <131977915+noraagrgl@users.noreply.github.com>
2024-05-26 21:46:18 +0200

    Update deconnexion.php

--- deconnexion.php
+++ deconnexion.php
@@ -1,8 +1,9 @@
 <?php
 
 	session_start();
+    // Détruit toutes les données de la session actuelle
 	session_destroy();
-
+    // Redirige l'utilisateur vers la page d'accueil
 	header("location:acceuil.html");
 	exit;
 
