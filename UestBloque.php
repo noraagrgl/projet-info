ff213a13918d05c9f5bcf04efbe3ae4e1e6f60b3
noraagrgl <131977915+noraagrgl@users.noreply.github.com>
2024-05-26 22:55:06 +0200

    Update UestBloque.php

--- UestBloque.php
+++ UestBloque.php
@@ -1,28 +1,25 @@
 <?php
 
-error_log("_______________________________________");
-
-// Vérifiez si l'ID utilisateur est défini dans les données POST
+// Vérifie si l'ID utilisateur est défini dans les données POST
 if (isset($_POST["UserId"])) {
     $destinataire = $_POST["UserId"];
     error_log("UserId reçu : " . $destinataire);
 } else {
-    // Vous pouvez définir un comportement par défaut ou retourner une erreur
-    error_log("UserId non défini dans les données POST");
+    // Si l'ID utilisateur n'est pas défini dans les données POST, retourne une erreur JSON
     echo json_encode(["bloqueSuccess" => false, "error" => "UserId non défini"]);
     exit;
 }
-
+// Ouvre le fichier contenant la liste des utilisateurs bloqués en mode lecture
 $fichierBlocage = fopen("data/blocage.txt", "r");
 
 $bloqueSuccess = false;
 
+// Vérifie si le fichier a été ouvert avec succès
 if ($fichierBlocage) {
     while (($ligne = fgets($fichierBlocage)) !== false) {
+        // Sépare les éléments de la ligne
         $utilisateurBloque = explode(";", trim($ligne));
-
-        error_log("destinataire = ".$destinataire."  utilisateurBloque = ".$utilisateurBloque[0]);
-
+        // Compare l'ID de l'utilisateur à bloquer avec celui de la ligne actuelle
         if ($destinataire === $utilisateurBloque[0]) {
             $bloqueSuccess = true;
             break;
