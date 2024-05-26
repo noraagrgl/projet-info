e4bc6f5c110f700935cd646059f814072b77718c
LegrosGabriel <133345446+LegrosGabriel@users.noreply.github.com>
2024-05-26 20:45:27 +0200

    Update UformulaireRecherche.php

--- UformulaireRecherche.php
+++ UformulaireRecherche.php
@@ -16,21 +16,37 @@ if (isset($_POST['recherchePseudo']) && strlen($_POST['recherchePseudo']) >= 3) 
     exit;
 }
 
+
+
+
+$fichier2 = fopen("data/bannissement.txt", "r");
+$bannis = [];
+while (($email = fgets($fichier2)) !== false) {
+    $bannis[] = trim($email);
+}
+fclose($fichier2);
+
+
+
+
 // Ouvrir le fichier txt
 $utilisateurs = array();
 if (($fichier = fopen("data/utilisateurs.txt", "r")) !== false) {
     while (($ligne = fgets($fichier)) !== false) {
 
         $ligne = explode(";", trim($ligne));
+
+        if (!in_array($ligne[0], $bannis)) {
         
-        if (stripos($ligne[2], $pseudoRecherche) === 0) {
-            // Vérifiez que la session contient une photo de profil
-            $photoProfil = isset($_SESSION['photo_profil']) && !empty($_SESSION['photo_profil']) ? 'image/'.$ligne[10] : 'image/default_photo.jpg';
-            $utilisateurs[] = array(
-                'pseudo' => $ligne[2],
-                'photo_profil' => $photoProfil,
-                'profile_url' => 'profile.php?user=' . urlencode($ligne[2]) // Assuming the URL format
-            );
+            if (stripos($ligne[2], $pseudoRecherche) === 0) {
+                // Vérifiez que la session contient une photo de profil
+                $photoProfil = isset($_SESSION['photo_profil']) && !empty($_SESSION['photo_profil']) ? 'image/'.$ligne[10] : 'image/default_photo.jpg';
+                $utilisateurs[] = array(
+                    'pseudo' => $ligne[2],
+                    'photo_profil' => $photoProfil,
+                    'email' => $ligne[0]
+                );
+            }
         }
     }
     fclose($fichier);
